01d7b91ec76b6d05746bfcef17f2049788666f6a
Pedro Henrique <pedroohrique@users.noreply.github.com>
2026-04-13 16:59:34 -0300

    Add_Vercel_Web_Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Header from "@/components/Header";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
                     {children}
                 </main>
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
